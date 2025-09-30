f647a58af28936b69b1b97aa933e9b055a204cca
Vladyslav <lokatheloka@gmail.com>
2025-09-30 20:21:18 +0200

    bugs

--- app/app.py
+++ app/app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-return 'Hello from my-devops-hello!'
+	return 'Hello from my-devops-hello!'
 
 
 if __name__ == '__main__':
-app.run(host='0.0.0.0', port=8080)
+	app.run(host='0.0.0.0', port=8080)
